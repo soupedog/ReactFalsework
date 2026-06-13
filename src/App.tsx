6c5a39f195a3222e193d35e51f47208966ef484f
Xavier.Wang <xavierpe@qq.com>
2026-06-13 12:42:54 +0800

    新增 pathParameter 和 queryParameter 使用示例

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import MusicPlayer from './pages/MusicPlayer.tsx'
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import AppLayout from "./pages/component/AppLayout.tsx";
+import Goods from "./pages/Goods.tsx";
+import GoodsDetail from "./pages/GoodsDetail.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -13,8 +16,11 @@ createRoot(document.getElementById('root')!).render(
                 <Route path={"/"} element={<AppLayout key={"layout"}/>}>
                     <Route index path={"/"} element={<Home key={"home"} text={""}/>}/>
                     <Route path={"/login"} element={<Login key={"login"}/>}/>
+                    <Route path={"/goods"} element={<Goods key={"goods"}/>}/>
+                    <Route path={"/goods/detail/:gid"} element={<GoodsDetail key={"detail"}/>}/>
+                    <Route path={"/musicPlayer"} element={<MusicPlayer key={"musicPlayer"}/>}/>
                     {/*从上到下匹配，上方全未匹配命中则说明该跳转到 musicPlayer 页面*/}
-                    <Route path={"*"} element={<MusicPlayer key={"musicPlayer"}/>}/>
+                    <Route path={"*"} element={<NotFound key={"notFound"} delayTime={3000}/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/pages/component/AppNavBar.tsx
+++ src/pages/component/AppNavBar.tsx
@@ -1,4 +1,4 @@
-import {NavLink, type NavLinkRenderProps} from "react-router-dom";
+import {Link, NavLink, type NavLinkRenderProps} from "react-router-dom";
 
 const navLinkStyle = ({isActive}: NavLinkRenderProps) => ({
     margin: "0 2rem",
@@ -10,9 +10,12 @@ const navLinkStyle = ({isActive}: NavLinkRenderProps) => ({
 export default function AppNavBar() {
     return (
         <nav>
+            <Link to={"/"}>普通 Link </Link>
             <NavLink to={"/"} style={navLinkStyle}>主页</NavLink>
-            <NavLink to={"/musicPlayer"} style={navLinkStyle}>音乐播放器</NavLink>
             <NavLink to={"/login"} style={navLinkStyle}>登录页</NavLink>
+            <NavLink to={"/goods"} style={navLinkStyle}>商品搜索页</NavLink>
+            <NavLink to={"/goods/detail/8"} style={navLinkStyle}>商品8</NavLink>
+            <NavLink to={"/musicPlayer"} style={navLinkStyle}>音乐播放器</NavLink>
         </nav>
     );
 }
